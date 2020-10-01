b64a99bf94d0e9f9db12f93ee7e722e1992e8518
natswatch <nathaliec@me.com>
2020-09-30 17:28:18 -0700

    prompt manager's info

--- index.js
+++ index.js
@@ -4,42 +4,60 @@ const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 
-function Team() {
+class Team {
+    constructor () {
     this.employees = [];
-};
+    }
 
-const promptManager = () => {
-    inquirer
-      .prompt([
-        {
-          type: 'input',
-          name: 'name',
-          message: "What is team manager's name?"
-        },
-        {
-          type: 'input',
-          name: 'id',
-          message: 'Please enter the employee ID'
-        },
-        {
-          type: 'input',
-          name: 'email',
-          message: 'Please enter the email address'
-        }
-    ])
-    .then(({name, id, email}) => {
-        const employee = new Employee(name,id, email);
-        console.log(employee);
-    })
-};
-
-
-
-
-const selectNextTeamMember = () => {
-
+    promptManager() {
+        inquirer
+          .prompt([
+            {
+              type: 'input',
+              name: 'name',
+              message: "What is team manager's name?"
+            },
+            {
+              type: 'input',
+              name: 'id',
+              message: 'Please enter the employee ID'
+            },
+            {
+              type: 'input',
+              name: 'email',
+              message: 'Please enter the email address'
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'Please enter the office number'
+              }
+        ])
+        .then(({name, id, email, officeNumber}) => {
+            this.employees.push(new Manager(name, id, email, officeNumber));
+            console.log(this.employees);
+        })
+    }
 }
 
-promptManager();
+const selectNextTeamMember = () => {
+    inquirer.prompt({
+        type: 'list',
+        message: 'Would you like to add a...',
+        name: 'role',
+        choices: ['Engineer', 'Intern', 'none']
+    })
+    .then(({action}) => {
+        if(action === 'Engineer'){
+
+        } else if(action === 'Intern') {
+            
+        } else {
+            return;
+        }
+    })
+}
+
+new Team().promptManager();
 
 module.exports = Team;
